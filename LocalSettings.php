1ddf5931aaa00aa7e4bf603ea04bdfde13e85151
nadat08 <nawaphondaiphonn@gmail.com>
2026-04-19 11:31:36 +0700

    Add LocalSettings.php with MariaDB configuration

---
+++ LocalSettings.php
@@ -0,0 +1,13 @@
+<?php
+
+$wgDBtype = 'mysql';
+$wgDBserver = 'localhost';
+$wgDBname = 'veripedia';
+$wgDBuser = 'db_user';
+$wgDBpassword = 'db_password';
+
+$wgDBtableoptions = 'ENGINE=InnoDB';
+
+$wgSecretKey = 'your_secret_key';
+$wgSiteKey = 'your_site_key';
+$wgSpamBlacklist = [];
